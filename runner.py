0de5f6a9f89c3b0ee1a920e4fef4aeef4f4cdb4f
Mike Lopez <dev@scidentify.info>
2013-10-08 02:40:39 -0400

    removed old views 1.4

--- runner.py
+++ runner.py
@@ -12,15 +12,15 @@ settings.configure(DEBUG=True,
                             'ENGINE': 'django.db.backends.sqlite3',
                         }
                   },
-                  ROOT_URLCONF='sciweb.apps.mainweb.urls',
+                  ROOT_URLCONF='mainweb.urls',
                   INSTALLED_APPS=('django.contrib.auth',
                                   'django.contrib.contenttypes',
                                   'django.contrib.sessions',
                                   'django.contrib.admin',
-                                  'sciweb.apps.mainweb',))
+                                  'mainweb',))
 
 from django.test.simple import DjangoTestSuiteRunner
 test_runner = DjangoTestSuiteRunner(verbosity=1)
-failures = test_runner.run_tests(['sciweb.apps.mainweb', ])
+failures = test_runner.run_tests(['mainweb', ])
 if failures:
     sys.exit(failures)
