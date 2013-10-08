f48f431b5cfcd254d279dd4d5acf7f7e92786a9f
Mike Lopez <dev@scidentify.info>
2013-10-08 04:27:42 -0400

    test model done

--- mainweb/tests/test_model.py
+++ mainweb/tests/test_model.py
@@ -37,7 +37,7 @@ class MainwebTestCase(TestCase):
                         title="My site", name="index",
                         template="index.html")
         wp.save()
-        self.assertEquals(wp.type, 'index')
+        self.assertEquals(getattr(wp, 'type'), "index")
 
     def test_sitepage_name_in_staticpages(self):
         """When the name of the sitepage is the name
@@ -46,7 +46,7 @@ class MainwebTestCase(TestCase):
                         title="My site", name="products",
                         template="index.html")
         wp.save()
-        self.assertEquals(wp.type, "static")
+        self.assertEquals(getattr(wp, 'type'), "static")
         
     def test_sitepage_name_in_staticargpages(self):
         """When the name of the sitepage is the name
@@ -56,7 +56,7 @@ class MainwebTestCase(TestCase):
                         title="My site", name="search",
                         template="index.html")
         wp.save()
-        self.assertTrue(wp.type, 'static-arg')
+        self.assertEquals(getattr(wp, 'type'), "static-arg")
 
     def test_sitepage_default_type(self):
         """Tests the default type for a sitepage given the 
@@ -65,6 +65,13 @@ class MainwebTestCase(TestCase):
                         title="My site", name="Somepage",
                         template="index.html")
         wp.save()
-        self.assertEquals(wp.type, "sub-landing")
+        self.assertEquals(getattr(wp, 'type'), "sub-landing")
 
-    
+ 
+    def test_website_domain_fix(self):
+        """Strips out any special characters from the url leaving
+        it as domainname.com from http://www.domainname.com
+        """
+        w = Website(domain="http://www.somewebsite.com")
+        w.save()
+        self.assertEquals(getattr(w, 'domain'), 'somewebsite.com')
